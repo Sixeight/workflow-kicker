5aebd3b84fae6c5cf57298f77667d154f6cece2e
Tomohiro Nishimura <tomohiro68@gmail.com>
2021-09-23 23:31:20 +0900

    Add tsconfig.json

--- app.ts
+++ app.ts
@@ -1,29 +1,31 @@
 const API_BASE = 'https://api.github.com';
 
-function dispatchWorkflow(
-  repository: string,
-  workflowId: string,
-  ref: string,
-  inputs: {[key: string]: unknown},
-  token: string
-) {
-  const path = `/repos/${repository}/actions/workflows/${workflowId}/dispatches`;
-  const payload = {
-    ref: ref,
-    inputs: inputs,
-  };
+class Workflow {
+  static dispatch(
+    repository: string,
+    workflowId: string,
+    ref: string,
+    inputs: {[key: string]: string},
+    token: string
+  ) {
+    const path = `/repos/${repository}/actions/workflows/${workflowId}/dispatches`;
+    const payload = {
+      ref: ref,
+      inputs: inputs,
+    };
 
-  apiRequest(path, payload, token);
-}
+    Workflow.apiRequest(path, payload, token);
+  }
 
-function apiRequest(path: string, payload: object, token: string) {
-  UrlFetchApp.fetch(API_BASE + path, {
-    method: 'post',
-    contentType: 'application/json',
-    headers: {
-      authorization: `token ${token}`,
-      accept: 'application/vnd.github.v3+json',
-    },
-    payload: JSON.stringify(payload),
-  });
+  private static apiRequest(path: string, payload: object, token: string) {
+    UrlFetchApp.fetch(API_BASE + path, {
+      method: 'post',
+      contentType: 'application/json',
+      headers: {
+        authorization: `token ${token}`,
+        accept: 'application/vnd.github.v3+json',
+      },
+      payload: JSON.stringify(payload),
+    });
+  }
 }
